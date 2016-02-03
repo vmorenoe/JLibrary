9f4395f247670eddaba69e8194c57804d75da8c2
Kaiz3r <Kaiz3r>
2016-02-03 17:25:40 +0100

    First commit

---
+++ WebContent/js/validacion.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+function validacion() {
+	var isbn= document.getElementById("isbn");
+	var miformulario=document.getElementById("miformulario");
+	if(isbn.value==""){   
+		alert("datos no validos");   
+		return false; 
+	}else{
+		miformulario.submit();
+	}
+}
